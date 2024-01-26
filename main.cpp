454cc341108fe6fdaeaf5a5ee4af0c63e9b66706
Maxim <feralfrenzy430@gmail.com>
2024-01-26 13:52:42 +0300

    version 2.5

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ std::map<std::string, session_data> sessions;
 
 httplib::Server::Handler loginСheck(httplib::Server::Handler next);
 void loginHandler(const Request& req, Response& res);
+void registerHandler(const Request& req, Response& res);
 void homeHandler(const Request& req, Response& res);
 void showHandler(const Request& req, Response& res);
 void getToHandler(const Request& req, Response& res);
@@ -38,6 +39,8 @@ int main()
     server.Post("/", loginСheck(homeHandler));
     server.Get("/login", loginСheck(loginHandler));
     server.Post("/login", loginСheck(loginHandler));
+    server.Get("/register", registerHandler);
+    server.Post("/register", registerHandler);
     server.Get("/show-short-link", loginСheck(showHandler));
     server.Get("/get-to", getToHandler);
     server.Get("/logout", loginСheck(logoutHandler));
@@ -59,21 +62,109 @@ static bool is_session_exist(const Request& req)
 httplib::Server::Handler loginСheck(httplib::Server::Handler next)
 {
     return [next](const Request& req, Response& res)
+    {
+        if (!is_session_exist(req) && req.path != "/login")
         {
-            if (!is_session_exist(req) && req.path != "/login")
-            {
-                res.set_redirect("/login");
-                return;
-            }
+            res.set_redirect("/login");
+            return;
+        }
 
-            if (is_session_exist(req) && req.path == "/login")
-            {
-                res.set_redirect("/");
-                return;
-            }
+        if (is_session_exist(req) && req.path == "/login")
+        {
+            res.set_redirect("/");
+            return;
+        }
 
-            next(req, res);
-        };
+        next(req, res);
+    };
+}
+
+
+std::map<std::string, std::string> registered_users;
+
+void registerHandler(const Request& req, Response& res)
+{
+    if (req.method == "GET")
+    {
+        std::string page = u8R"#(
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Sign up</title>
+                <style>
+                    body {
+                        background-color: #f0f0f0;
+                        font-family: Arial, sans-serif;
+                    }
+                    .register-panel {
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        width: 500px;
+                        transform: translate(-50%, -50%);
+                        padding: 20px;
+                        background-color: #fff;
+                        box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
+                        border-radius: 10px;
+                    }
+                    label {
+                        display: inline-block;
+                        margin-bottom: 5px;
+                    }
+                    input[type="text"], input[type="password"] {
+                        width: 94%;
+                        padding: 10px;
+                        margin-bottom: 10px;
+                        border-radius: 5px;
+                        border: 1px solid #ccc;
+                    }
+                    .btn-new {
+                        width: 70px;
+                        height: 35px;
+                        border-radius: 10px;
+                        color: white;
+                        transition: .2s linear;
+                        background: #0B63F6;
+                    }
+                    .btn-new:hover {
+                        box-shadow: 0 0 0 2px white, 0 0 0 4px #3C82F8;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="register-panel">
+                    <form action="/register" method="post">
+                        <label for="login"></label>
+                        <input type="text" placeholder="Создайте логин или введите адрес почты" name="login" autocomplete="off" required><br>
+                        <label for="password"></label>
+                        <input type="password" placeholder="Придумайте пароль" autocomplete="off" name="password" required><br>
+                        <label for="confirmed_password"></label>
+                        <input type="password" placeholder="Подтвердите пароль" autocomplete="off" name="confirmed_password" required><br>
+                        <button class="btn-new" type="submit">Далее</button>
+                    </form>
+                </div>
+            </body>
+            </html>
+        )#";
+
+        res.set_content(page, "text/html");
+    }
+
+    if (req.method == "POST")
+    {
+        auto new_login = req.has_param("login") ? req.get_param_value("login") : "";
+        auto new_password = req.has_param("password") ? req.get_param_value("password") : "";
+        auto confirmed_password = req.has_param("confirmed_password") ? req.get_param_value("confirmed_password") : "";
+
+        if (!new_login.empty() && (!new_password.empty() && new_password == confirmed_password))
+        {
+            registered_users[new_login] = confirmed_password;
+            std::cout << "Registered user " << new_login << " with password " << confirmed_password << std::endl;
+            res.set_redirect("/login");
+        }
+    }
 }
 
 void loginHandler(const Request& req, Response& res)
@@ -102,6 +193,14 @@ void loginHandler(const Request& req, Response& res)
                         box-shadow: 0px 0px 10px rgba(0,0,0,0.1);
                         border-radius: 10px;
                     }
+                    .register-div {
+                        position: fixed;
+                        top: 0;
+                        width: 100%;
+                        height: 5vh;
+                        display: flex;
+                        justify-content: space-around;
+                    }
                     label {
                         display: inline-block;
                         margin-bottom: 5px;
@@ -114,7 +213,7 @@ void loginHandler(const Request& req, Response& res)
                         border: 1px solid #ccc;
                     }
                     .btn-new {
-                        width: 60px;
+                        width: 70px;
                         height: 35px;
                         border-radius: 10px;
                         color: white;
@@ -129,13 +228,19 @@ void loginHandler(const Request& req, Response& res)
             <body>
 		        <form method="post" action="/login" accept-charset="UTF-8">
 				    <div class="mainContainer">
-						<label for="login"></label>
-						<input type="text" autocomplete="off" placeholder="Введите логин" name="login" required>
-						<label for="password"></label>
-                        <input type="password" autocomplete="off" placeholder="Введите пароль" name="password" required>
-						<button class="btn-new" type="submit">Вход</button>
-				    </div>
+			            <label for="login"></label>
+			            <input type="text" autocomplete="off" placeholder="Введите логин" name="login">
+			            <label for="password"></label>
+                        <input type="password" autocomplete="off" placeholder="Введите пароль" name="password">
+			            <button class="btn-new" type="submit">Вход</button>
+	                </div>
 		        </form>
+                <div class="/register-div">
+                    <p>Нет аккаунта? Создай!</p>
+                    <form action="/register" method="get">
+                        <button class="btn-new" type="submit">Создать</button>
+                    </form>
+                </div>
             </body>
             </html>)#";
 
@@ -144,12 +249,10 @@ void loginHandler(const Request& req, Response& res)
 
     if (req.method == "POST")
     {
-        const std::string correct_password = "15123415";
-
         auto login = req.has_param("login") ? req.get_param_value("login") : "";
         auto password = req.has_param("password") ? req.get_param_value("password") : "";
 
-        if (login != "" && password == correct_password)
+        if (registered_users.find(login) != registered_users.end() && registered_users[login] == password)
         {
             Cookie cookie;
             cookie.name = "user_cookie";
@@ -182,41 +285,71 @@ void homeHandler(const Request& req, Response& res)
         std::string page = u8R"#(
             <!DOCTYPE html>
             <html>
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Home</title>
-                    <style>
-                        .main-box {
-                            border: 1px solid;
-                            border-radius: 3px;
-                            position: absolute;
-                            top: 50 %;
-                            left: 50 %;
-                            transform: translate(-50 %, -50 %);
-                            padding: 10px;
-                        }
-                        .inp-str {
-                            border: 1px solid;
-                            border-radius: 2px;
-                        }
-                        .sbmt {
-                            border: 1px;
-                            border-radius: 3px;
-                            color: #FFF;
-                            background-color: #0000FF;
-                        }
-                    </style>
-                </head>
-                <body>
-                    <div class="main-box">
-                        <form action="/" method="post">
-                            <label for="long-link">
-                                <input class="inp-str" type="url" name="long-link" placeholder="Enter long URL" autocomplete="off">
-                                <button class="sbmt" type="submit">Shorten</button>
-                            </label>
-                        </form>
-                    </div>
-                </body>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Home</title>
+                <style>
+                    body {
+                        background-color: #f0f0f0;
+                        font-family: Arial, Helvetica, sans-serif;
+                    }
+                    .main-box {
+                        border: 1px solid;
+                        border-radius: 3px;
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        padding: 10px;
+                    }
+                    .upper-panel {
+                        position: fixed;
+                        top: 0;
+                        width: 100%;
+                        height: 5vh;
+                        display: flex;
+                        justify-content: space-around;
+                        background-color: #808080;
+                    }
+                    .panel-btn {
+                        border: none;
+                        border-radius: 5px;
+                        width: 80px;
+                        height: 35px;
+                        margin-top: 6px;
+                    }
+                    .inp-str {
+                        border: 1px solid;
+                        border-radius: 2px;
+                        width: 500px;
+                    }
+                    .sbmt {
+                        border: 1px;
+                        border-radius: 3px;
+                        color: #FFF;
+                        background-color: #0000FF;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="upper-panel">
+                    <form action="/get-history" method="get">
+                        <button class="panel-btn" type="submit">История</button>
+                    </form>
+                    <form action="/logout" method="get">
+                        <button class="panel-btn" type="submit">Выход</button>
+                    </form>
+                </div>
+                <div class="main-box">
+                    <form action="/" method="post">    
+                        <label for="long-link">
+                            <input class="inp-str" type="url" name="long-link" placeholder="Введите длинную ссылку" autocomplete="off">
+                            <button class="sbmt" type="submit">Сократить</button>
+                        </label>
+                    </form>
+                </div>
+            </body>
             </html>
         )#";
         
@@ -236,7 +369,7 @@ void showHandler(const Request& req, Response& res)
 {
     std::string page;
     auto id = req.get_param_value("id");
-    page += u8"<p>26.83.106.113:8080/get-to?id=" + id;
+    page += u8"<p>localhost:8080/get-to?id=" + id;
 
     res.set_content(page, "text/html");
 }
